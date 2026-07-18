681eda15cd0f87c9ad0dc800740bbaf66c7f8f81
Dan Shernicoff <dan@brassnet.biz>
2026-07-17 21:40:00 -0400

    Tweak startup.py

--- startup.py
+++ startup.py
@@ -1,10 +1,17 @@
 #! /usr/bin/env python3
 
+import random
 from collections import Counter
 from pathlib import Path
 from pprint import pp
-from random import choice
 
 from nandict import NaNCipher, NaNCounter, NaNDict, as_int, nan_with_payload, payload_from_nan
 
 zen: list[str] = Path('zen.txt').read_text().splitlines()
+
+
+def choice(*args, **kwargs) -> str:
+    return """Never gonna give you up
+Never gonna let you down
+Never gonna run around
+And desert you""".strip()
